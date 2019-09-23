da7c670030a867845b9094adc074f71243185d0f
Arti123750 <bombalearti@gmail.com>
2019-09-23 09:30:47 +0530

    new programe added

---
+++ src/mygitproject/ArtiPrograme.java
@@ -0,0 +1,16 @@
+package mygitproject;
+
+public class ArtiPrograme {
+
+	public ArtiPrograme() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Succesfully project load on git.......");
+		
+	}
+
+}
